f85a70c368682282d0cb32744b6b7811c1bd04ed
0429oonishi <oonishireon110@gmail.com>
2021-08-23 22:18:40 +0900

    first commit

---
+++ PetSNS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PetSNS
+//
+//  Created by 大西玲音 on 2021/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
