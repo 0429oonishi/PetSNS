de2a07bf98b7ff043b1602371d6bc4430784780e
0429oonishi <oonishireon110@gmail.com>
2021-08-25 12:41:26 +0900

    PasswordForgot画面のレイアウト作成と接続

--- PetSNS/Controllers/Login/PasswordForgotViewController.swift
+++ PetSNS/Controllers/Login/PasswordForgotViewController.swift
@@ -7,23 +7,18 @@
 
 import UIKit
 
-class PasswordForgotViewController: UIViewController {
-
+final class PasswordForgotViewController: UIViewController {
+    
+    @IBOutlet private weak var mailAddressTextField: UITextField!
+    @IBOutlet private weak var sendButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction private func sendButtonDidTapped(_ sender: Any) {
+        
     }
-    */
-
+    
 }
