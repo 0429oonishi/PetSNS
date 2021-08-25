d807305d35da80c0ec47c5d146b8563d83db5a25
sagaraEito <gjmptw.aab@gmail.com>
2021-08-25 20:30:53 +0900

    Login画面のレイアウト作成と接続完了

--- PetSNS/Controllers/Login/LoginViewController.swift
+++ PetSNS/Controllers/Login/LoginViewController.swift
@@ -7,23 +7,19 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
+final class LoginViewController: UIViewController {
+
+    @IBOutlet private weak var mailAddressTextField: UITextField!
+    @IBOutlet private weak var passwordTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction private func registerButtonDidTapped(_ sender: Any) {
     }
-    */
+
+    @IBAction private func passwordForgotDidTapped(_ sender: Any) {
+    }
 
 }
