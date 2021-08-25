60605cdf85848ae6b59dcf3c1d0e9d1cb297240b
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2021-08-25 18:19:31 +0900

    SignUp画面のレイアウト作成と接続完了

--- PetSNS/Controllers/Login/SignUpViewController.swift
+++ PetSNS/Controllers/Login/SignUpViewController.swift
@@ -7,23 +7,20 @@
 
 import UIKit
 
-class SignUpViewController: UIViewController {
-
+final class SignUpViewController: UIViewController {
+    
+    @IBOutlet private weak var mailAddressTextField: UITextField!
+    @IBOutlet private weak var confirmationMailAddressTextField: UITextField!
+    @IBOutlet private weak var passwordTextField: UITextField!
+    @IBOutlet private weak var confirmationPasswordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction private func registerButtonDidTapped(_ sender: Any) {
+        
     }
-    */
-
+    
 }
