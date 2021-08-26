2eea79e6c745874c51f489116601843974948f69
sagaraEito <gjmptw.aab@gmail.com>
2021-08-26 15:44:16 +0900

    navigationbarとソースコードの修正

--- PetSNS/Controllers/Home/PostDetailViewController.swift
+++ PetSNS/Controllers/Home/PostDetailViewController.swift
@@ -12,27 +12,8 @@ final class PostDetailViewController: UIViewController {
     @IBOutlet private weak var postView: UIView!
     @IBOutlet private weak var commentTableView: UITableView!
 
-    private let cellID = "PostDetailViewController"
-
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        commentTableView.delegate = self
-        commentTableView.dataSource = self
-    }
-
-}
-
-extension PostDetailViewController: UITableViewDelegate, UITableViewDataSource {
-
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        1
-    }
-
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath)
-
-        return cell
     }
 
 }
