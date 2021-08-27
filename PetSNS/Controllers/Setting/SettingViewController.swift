f15faf419193729d15a5f5aea53e2c17a3e2add5
0429oonishi <oonishireon110@gmail.com>
2021-08-27 13:55:16 +0900

    設定画面のレイアウトと接続

--- PetSNS/Controllers/Setting/SettingViewController.swift
+++ PetSNS/Controllers/Setting/SettingViewController.swift
@@ -7,23 +7,22 @@
 
 import UIKit
 
-class SettingViewController: UIViewController {
-
+final class SettingViewController: UIViewController {
+    
+    @IBOutlet private weak var tableView: UITableView!
+    @IBOutlet private weak var logoutButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction private func closeButtonDidTapped(_ sender: Any) {
+        
     }
-    */
-
+    
+    @IBAction private func logoutButtonDidTapped(_ sender: Any) {
+        
+    }
+    
 }
