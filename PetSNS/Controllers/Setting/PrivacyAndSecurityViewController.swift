743515d9c22cff85278be5dfdfffc5a68969c9bd
0429oonishi <oonishireon110@gmail.com>
2021-08-27 13:59:09 +0900

    プライバシーとセキュリティ画面のレイアウトと接続

--- PetSNS/Controllers/Setting/PrivacyAndSecurityViewController.swift
+++ PetSNS/Controllers/Setting/PrivacyAndSecurityViewController.swift
@@ -7,23 +7,13 @@
 
 import UIKit
 
-class PrivacyAndSecurityViewController: UIViewController {
-
+final class PrivacyAndSecurityViewController: UIViewController {
+    
+    @IBOutlet private weak var textView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
