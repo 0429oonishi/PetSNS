76807be9e102789842358a5c15d51d5eeb3b7c85
sagaraEito <gjmptw.aab@gmail.com>
2021-08-28 15:33:00 +0900

    修正。階層分け、レイアウトの変更、プロパティ名変換

--- PetSNS/Views/Xibs/Home/HomePostTableViewCell.swift
+++ PetSNS/Views/Xibs/Home/HomePostTableViewCell.swift
@@ -7,22 +7,14 @@
 
 import UIKit
 
-final class HomeTableViewCell: UITableViewCell {
+final class HomePostTableViewCell: UITableViewCell {
 
     @IBOutlet private weak var profileImageView: UIImageView!
     @IBOutlet private weak var userNameLabel: UILabel!
     @IBOutlet private weak var followButton: UIButton!
-    @IBOutlet private weak var postImageView: UIImageView!
+    @IBOutlet private weak var postImageOrMovieView: UIView!
     @IBOutlet private weak var favoriteButton: UIButton!
     @IBOutlet private weak var postTextView: UITextView!
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-    }
 
     @IBAction private func followButtonDidTapped(_ sender: Any) {
     }
