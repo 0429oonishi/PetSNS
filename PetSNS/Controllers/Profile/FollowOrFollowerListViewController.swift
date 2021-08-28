c7eac0ab35bf4067b5ddd59b4ed77633055f0ab4
Reon0429-cat <oonishireon110@gmail.com>
2021-08-28 14:35:21 +0900

    ペット一覧のカスタムセルを作成と接続

--- PetSNS/Controllers/Profile/FollowOrFollowerListViewController.swift
+++ PetSNS/Controllers/Profile/FollowOrFollowerListViewController.swift
@@ -14,6 +14,43 @@ final class FollowOrFollowerListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupTableView()
+        
+    }
+    
+    private func setupTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(PetInfoTableViewCell.nib,
+                           forCellReuseIdentifier: PetInfoTableViewCell.identifier)
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.tableFooterView = UIView()
+    }
+    
+}
+
+extension FollowOrFollowerListViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView,
+                   heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return tableView.rowHeight
+    }
+    
+}
+
+extension FollowOrFollowerListViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView,
+                   numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView,
+                   cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(
+            withIdentifier: PetInfoTableViewCell.identifier
+        ) as! PetInfoTableViewCell
+        return cell
     }
     
 }
---
+++ PetSNS/Views/PetInfoTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  PetInfoTableViewCell.swift
+//  PetSNS
+//
+//  Created by 大西玲音 on 2021/08/28.
+//
+
+import UIKit
+
+final class PetInfoTableViewCell: UITableViewCell {
+    
+    @IBOutlet private weak var petImageView: UIImageView!
+    @IBOutlet private weak var petNameLabel: UILabel!
+    
+    static var identifier: String { String(describing: self) }
+    static var nib: UINib { UINib(nibName: String(describing: self), bundle: nil) }
+    
+}
