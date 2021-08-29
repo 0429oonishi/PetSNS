cafcb9fa67c7b442ae0c60cb123f7a37e8cb878a
Reon0429-cat <oonishireon110@gmail.com>
2021-08-29 23:42:05 +0900

    NotificationName+Extensionファイル作成

---
+++ PetSNS/Extensions/NotificationName+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  NotificationName+Extension.swift
+//  PetSNS
+//
+//  Created by 大西玲音 on 2021/08/29.
+//
+
+import Foundation
+
+extension Notification.Name {
+    
+    static let showHomeVC = Notification.Name("showHomeVC")
+    
+}
