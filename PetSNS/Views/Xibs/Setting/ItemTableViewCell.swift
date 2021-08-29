18cd0e2a804f7d7a5234c08c781c7678981a2741
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2021-08-29 14:33:07 +0900

    Setting画面の項目表示のカスタムセルを作成しました。

---
+++ PetSNS/Views/Xibs/Setting/ItemTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  ItemTableViewCell.swift
+//  PetSNS
+//
+//  Created by 坂本龍哉 on 2021/08/29.
+//
+
+import UIKit
+
+final class ItemTableViewCell: UITableViewCell {
+
+    @IBOutlet private weak var itemLabel: UILabel!
+    
+    static var identifier: String { String(describing: self) }
+    static var nib: UINib { UINib(nibName: String(describing: self), bundle: nil) }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
