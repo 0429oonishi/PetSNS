f6be3fa0ba5538cb7f8c95b3e542e7199df41044
Reon0429-cat <oonishireon110@gmail.com>
2021-08-29 20:56:22 +0900

    UIViewController+ExtensionにVC生成メソッドを追加

---
+++ PetSNS/Extensions/UIViewController+Extension.swift
@@ -0,0 +1,26 @@
+//
+//  UIViewController+Extension.swift
+//  PetSNS
+//
+//  Created by 大西玲音 on 2021/08/29.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    static func instantiate() -> Self {
+        var storyboardName = String(describing: self)
+        if let result = storyboardName.range(of: "ViewController") {
+            storyboardName.removeSubrange(result)
+        } else {
+            fatalError("Storyboardの名前が正しくない")
+        }
+        let storyboard = UIStoryboard(name: storyboardName, bundle: nil)
+        let vc = storyboard.instantiateViewController(
+            identifier: String(describing: self)
+        ) as! Self
+        return vc
+    }
+    
+}
