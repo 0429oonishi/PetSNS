f18953e5af2779157e65d09d176cf00cbb34a2eb
Reon0429-cat <oonishireon110@gmail.com>
2021-08-31 14:09:46 +0900

    Indicatorリファクタリング

--- PetSNS/Models/Indicator.swift
+++ PetSNS/Models/Indicator.swift
@@ -5,12 +5,19 @@
 //  Created by 大西玲音 on 2021/08/31.
 //
 
-import PKHUD 
+import PKHUD
+
+enum IndicatorType {
+    case progress
+    case success
+    case error
+}
 
 struct Indicator {
     
-    func flash(_ type: HUDContentType,
-                  completion: @escaping () -> Void) {
+    func flash(_ type: IndicatorType,
+               completion: @escaping () -> Void) {
+        let type = convertIndicatorType(from: type)
         HUD.flash(type,
                   onView: nil,
                   delay: 0) { _ in
@@ -18,12 +25,21 @@ struct Indicator {
         }
     }
     
-    func show(_ type: HUDContentType) {
+    func show(_ type: IndicatorType) {
+        let type = convertIndicatorType(from: type)
         HUD.show(type)
     }
     
     func hide() {
         HUD.hide()
+    }
+    
+    private func convertIndicatorType(from type: IndicatorType) -> HUDContentType {
+        switch type {
+            case .progress: return .progress
+            case .success: return .success
+            case .error: return .error
+        }
     }
     
 }
