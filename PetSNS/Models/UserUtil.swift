87ab0fa423299edc381cbd066b7d23d21bfea678
Reon0429-cat <oonishireon110@gmail.com>
2021-09-02 20:21:17 +0900

    logout実装

--- PetSNS/Models/UserUtil.swift
+++ PetSNS/Models/UserUtil.swift
@@ -43,7 +43,16 @@ final class UserUtil {
             }
             completion(.success(nil))
         }
-        
+    }
+    
+    func logout(completion: @escaping ResultHandler<Any?>) {
+        do {
+            try Auth.auth().signOut()
+            completion(.success(nil))
+        } catch {
+            let message = authErrorMessage(error)
+            completion(.failure(message))
+        }
     }
     
     private func authErrorMessage(_ error: Error) -> String {
