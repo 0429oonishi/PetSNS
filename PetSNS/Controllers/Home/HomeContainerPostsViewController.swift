e9bad3b8ea40fef164776815aa696b7af47d3978
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2021-09-04 21:36:12 +0900

    投稿画面切り替えの処理作成

--- PetSNS/Controllers/Home/HomeContainerPostsViewController.swift
+++ PetSNS/Controllers/Home/HomeContainerPostsViewController.swift
@@ -9,21 +9,30 @@ import UIKit
 
 final class HomeContainerPostsViewController: UIViewController {
     
-    @IBOutlet private weak var tableView: UITableView!
+    @IBOutlet private weak var allTableView: UITableView!
+    @IBOutlet private weak var followingTableView: UITableView!
+    @IBOutlet private weak var horizontalScrollView: UIScrollView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupTableView()
+        setupScrollView()
         
     }
     
     private func setupTableView() {
-        tableView.delegate = self
-        tableView.dataSource = self
-        tableView.rowHeight = UITableView.automaticDimension
-        tableView.register(HomePostTableViewCell.nib,
-                           forCellReuseIdentifier: HomePostTableViewCell.identifier)
+        [allTableView, followingTableView]
+            .forEach {
+                $0?.delegate = self
+                $0?.dataSource = self
+                $0?.rowHeight = UITableView.automaticDimension
+                $0?.register(HomePostTableViewCell.nib,
+                             forCellReuseIdentifier: HomePostTableViewCell.identifier) }
+    }
+    
+    private func setupScrollView() {
+        horizontalScrollView.delegate = self
     }
     
 }
@@ -35,6 +44,14 @@ extension HomeContainerPostsViewController: UITableViewDelegate {
         return UITableView.automaticDimension
     }
     
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        guard let homeVC = parent as? HomeViewController else { return }
+        if (scrollView == self.horizontalScrollView) {
+            let scrollOffset = scrollView.contentOffset.x / 2
+            homeVC.moveSelectedMarkView(contentOffset: scrollOffset)
+            print(scrollOffset)
+        }
+    }
 }
 
 extension HomeContainerPostsViewController: UITableViewDataSource {
