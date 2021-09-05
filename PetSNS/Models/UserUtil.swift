3dd3016a61cdeef46ab9dc6719c8ca557ef6c56e
sagaraEito <gjmptw.aab@gmail.com>
2021-09-06 02:02:11 +0900

    登録処理とindicatorの追加

--- PetSNS/Models/UserUtil.swift
+++ PetSNS/Models/UserUtil.swift
@@ -31,6 +31,20 @@ final class UserUtil {
         }
     }
     
+    func signUp(email: String, 
+                password: String, 
+                completion: @escaping ResultHandler<Any?>) {
+        Auth.auth().createUser(withEmail: email, 
+                               password: password) { authResult, error in
+            if let error = error {
+                let message = self.authErrorMessage(error)
+                completion(.failure(message))
+                return
+            }
+            completion(.success(nil))
+        }
+    }
+    
     func login(email: String,
                password: String,
                completion: @escaping ResultHandler<Any?>) {
@@ -58,15 +72,14 @@ final class UserUtil {
     private func authErrorMessage(_ error: Error) -> String {
         if let errorCode = AuthErrorCode(rawValue: error._code) {
             switch errorCode {
-                case .invalidEmail: return "メールアドレスの形式に誤りが含まれます。"
-                case .weakPassword: return "パスワードは６文字以上で入力してください。"
-                case .wrongPassword: return "パスワードに誤りがあります。"
-                case .userNotFound: return "こちらのメールアドレスは登録されていません。"
-                case .emailAlreadyInUse: return "こちらのメールアドレスは既に登録されています。"
-                default: return "ログインに失敗しました\(error)"
+            case .invalidEmail: return "メールアドレスの形式に誤りが含まれます。"
+            case .weakPassword: return "パスワードは６文字以上で入力してください。"
+            case .wrongPassword: return "パスワードに誤りがあります。"
+            case .userNotFound: return "こちらのメールアドレスは登録されていません。"
+            case .emailAlreadyInUse: return "こちらのメールアドレスは既に登録されています。"
+            default: return "ログインに失敗しました\(error)"
             }
         }
         return "不明なエラーが発生しました。"
     }
-    
 }
