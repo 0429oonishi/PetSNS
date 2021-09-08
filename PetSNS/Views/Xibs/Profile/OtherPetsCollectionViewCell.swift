1a760cd84a6faeb5fdaff3cf655a5261e14fd709
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2021-09-08 17:44:31 +0900

    他のペットを表示するCollectionViewCellのXibを作成

---
+++ PetSNS/Views/Xibs/Profile/OtherPetsCollectionViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  OtherPetsCollectionViewCell.swift
+//  PetSNS
+//
+//  Created by 坂本龍哉 on 2021/09/08.
+//
+
+import UIKit
+
+final class OtherPetsCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet private weak var otherPetsImageView: UIImageView!
+    
+    static var identifier: String { String(describing: self) }
+    static var nib: UINib { UINib(nibName: String(describing: self), bundle: nil) }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+    }
+
+}
